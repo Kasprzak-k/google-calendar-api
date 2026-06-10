98c1cd82c76337e86d66c903b114f6cf6c6a4a63
Kasprzak-k <kasprzakramos@icloud.com>
2026-06-10 19:50:43 -0300

    refactor: adjust EventService injection and update calendar target to brazilian holidays
    Remove Lombok @RequiredArgsConstructor para permitir injecao da api key via @Value no construtor. Atualiza a URI da API do Google Calendar para buscar o calendario de feriados do Brasil.

--- src/main/java/com/google/calendar/api_google_calendar/Service/EventService.java
+++ src/main/java/com/google/calendar/api_google_calendar/Service/EventService.java
@@ -5,25 +5,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import com.google.calendar.api_google_calendar.dto.EventResponse;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-// era @Data porem, é inadequado: services geralmente não têm estado mutável
-// para expor via setters. O correto seria usar:
-@RequiredArgsConstructor
-
 public class EventService {
     private final RestClient restClient;
+    private final String apiKey;
 
-    @Value("${google.api.key}")
-    private String apiKey;
+    public EventService(RestClient restClient, @Value("{google.api-key}") String apiKey) {
+        this.apiKey = apiKey;
+        this.restClient = restClient;
+    }
 
     public EventResponse getEvent() {
         return restClient.get()
-                .uri("https://content.googleapis.com/calendar/v3/calendars/primary/events?orderBy=startTime&singleEvents=true&timeMax=2026-12-31T00%3A00%3A00Z&timeMin=2026-01-01T00%3A00%3A00Z&fields=items(summary%2Cstart%2Cend)&key=" + apiKey)
+                .uri("https://content.googleapis.com/calendar/v3/calendars/pt.brazilian%23holiday@group.v.calendar.google.com/events?orderBy=startTime&singleEvents=true&timeMax=2026-12-31T00%3A00%3A00Z&timeMin=2026-01-01T00%3A00%3A00Z&fields=items(summary%2Cstart%2Cend)&key="
+                        + apiKey)
                 .retrieve()
                 .body(EventResponse.class);
-
     }
-
 }
