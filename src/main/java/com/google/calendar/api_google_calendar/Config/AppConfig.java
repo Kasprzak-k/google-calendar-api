b44f82667174babb39037e984e7ceef16f0796b7
Kasprzak <kasprzakramos@icloud.com>
2026-07-27 19:20:13 -0300

    feat: adiciona configuracao AppConfig com Bean do RestClient

---
+++ src/main/java/com/google/calendar/api_google_calendar/Config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.google.calendar.api_google_calendar.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl("https://www.googleapis.com/calendar/v3")
+                .defaultHeader("Authorization", "Bearer " + apiKey)
+                .build();
+    }
++}
