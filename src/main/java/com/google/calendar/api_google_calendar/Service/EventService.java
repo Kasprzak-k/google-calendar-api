0f1a46aa3b420ed6bb5c7e87a97123abc203034f
Kasprzak <kasprzakramos@icloud.com>
2026-07-28 19:28:57 -0300

    fix: corrige @Value, URL da API do Google Calendar e nome da propriedade

--- src/main/java/com/google/calendar/api_google_calendar/Service/EventService.java
+++ src/main/java/com/google/calendar/api_google_calendar/Service/EventService.java
@@ -10,14 +10,14 @@ public class EventService {
     private final RestClient restClient;
     private final String apiKey;
 
-    public EventService(RestClient restClient, @Value("{google.api-key}") String apiKey) {
+    public EventService(RestClient restClient, @Value("${google.api.key}") String apiKey) {
         this.apiKey = apiKey;
         this.restClient = restClient;
     }
 
     public EventResponse getEvent() {
         return restClient.get()
-                .uri("https://content.googleapis.com/calendar/v3/calendars/pt.brazilian%23holiday@group.v.calendar.google.com/events?orderBy=startTime&singleEvents=true&timeMax=2026-12-31T00%3A00%3A00Z&timeMin=2026-01-01T00%3A00%3A00Z&fields=items(summary%2Cstart%2Cend)&key="
+                .uri("https://www.googleapis.com/calendar/v3/calendars/pt.brazilian%23holiday@group.v.calendar.google.com/events?orderBy=startTime&singleEvents=true&timeMax=2026-12-31T00%3A00%3A00Z&timeMin=2026-01-01T00%3A00%3A00Z&fields=items(summary%2Cstart%2Cend)&key="
                         + apiKey)
                 .retrieve()
                 .body(EventResponse.class);
