644ffddb861f9a6990c45f8bbb692ffa7e25d215
Kasprzak <kasprzakramos@icloud.com>
2026-07-28 15:30:58 -0300

    chore: remove dependencias de banco de dados (JPA e PostgreSQL)

--- src/main/java/com/google/calendar/api_google_calendar/Repository/EventRepository.java
+++ src/main/java/com/google/calendar/api_google_calendar/Repository/EventRepository.java
@@ -1,12 +1,8 @@
 package com.google.calendar.api_google_calendar.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.google.calendar.api_google_calendar.Entity.Event;
-
 @Repository
-public interface EventRepository
-        extends JpaRepository<Event, Long> {
+public interface EventRepository {
 
 }
